357ee76e7e8e51073f3b6b578696e88f891702a1
Luiza12 <91795069+LuizaPG12@users.noreply.github.com>
2021-10-08 12:03:33 -0300

    Add files via upload

---
+++ super-trunfo-she-ra/super-trunfoshe-ra/README.markdown
@@ -0,0 +1,5 @@
+# Super Trunfo - She-Ra
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Luiza12/pen/QWgJpoL](https://codepen.io/Luiza12/pen/QWgJpoL).
+
+Super Trunfo da She-Ra e as princesas do poder. Sorteie a carta e escolha o atributo para vencer!
---
+++ super-trunfo-she-ra/super-trunfoshe-ra/src/script.js
@@ -0,0 +1,604 @@
+// Deck de cartas
+
+var carta1 = {
+  nome: "She-Ra",
+  fala: "For the Honor of Grayskull!",
+  imagem: "https://c.tenor.com/F3UQ3iDuxRoAAAAd/shera-adora.gif",
+  atributos: {
+    Ataque: 1000,
+    Defesa: 900,
+    Magia: 1000,
+    Inteligência: 950
+  }
+};
+
+var carta2 = {
+  nome: "Adora",
+  fala: "Boop, boop, boop, boop!",
+  imagem: "https://filmdaze.net/wp-content/uploads/2020/05/TE2ygmIi.jpg",
+  atributos: {
+    Ataque: 700,
+    Defesa: 650,
+    Magia: 000,
+    Inteligência: 900
+  }
+};
+
+var carta3 = {
+  nome: "Catra",
+  fala: "Hey, Adora.",
+  imagem:
+    "https://pbs.twimg.com/profile_images/1312514313408573441/3UXdREKp.jpg",
+  atributos: {
+    Ataque: 700,
+    Defesa: 600,
+    Magia: 000,
+    Inteligência: 900
+  }
+};
+
+var carta4 = {
+  nome: "Glimmer",
+  fala: "It's time for us to take back our home.",
+  imagem:
+    "https://i.pinimg.com/originals/a4/af/11/a4af11f0a951545eaa628b92421336e7.jpg",
+  atributos: {
+    Ataque: 800,
+    Defesa: 700,
+    Magia: 900,
+    Inteligência: 850
+  }
+};
+
+var carta5 = {
+  nome: "Bow",
+  fala: "Best friend squad!",
+  imagem:
+    "https://i.pinimg.com/originals/80/5c/fb/805cfb8905b809f4c6250a392a795b81.jpg",
+  atributos: {
+    Ataque: 600,
+    Defesa: 700,
+    Magia: 000,
+    Inteligência: 900
+  }
+};
+
+var carta6 = {
+  nome: "Angella",
+  fala: "Take care of each other.",
+  imagem:
+    "https://i.pinimg.com/564x/34/d4/8c/34d48c872bbc267dad6ed1c3ae3b0af0.jpg",
+  atributos: {
+    Ataque: 600,
+    Defesa: 600,
+    Magia: 900,
+    Inteligência: 900
+  }
+};
+
+var carta7 = {
+  nome: "Castaspella",
+  fala: "You've come at the perfect time! ",
+  imagem:
+    "https://i.pinimg.com/originals/01/5a/6e/015a6e1c9be2fbd547a389ee1e29a537.jpg",
+  atributos: {
+    Ataque: 500,
+    Defesa: 500,
+    Magia: 800,
+    Inteligência: 800
+  }
+};
+
+var carta8 = {
+  nome: "Double Trouble",
+  fala: "Your face right now is almost better than applause.",
+  imagem:
+    "https://i.pinimg.com/564x/b6/9f/1b/b69f1be3d911993ca4dccf3dd58be3f6.jpg",
+  atributos: {
+    Ataque: 200,
+    Defesa: 500,
+    Magia: 000,
+    Inteligência: 800
+  }
+};
+
+var carta9 = {
+  nome: "Entrapta",
+  fala: "Imperfection Is Beautiful...At Least To Me.",
+  imagem:
+    "https://i.pinimg.com/736x/22/d7/90/22d7902f31f3cb4737e2a6b505bde585.jpg",
+  atributos: {
+    Ataque: 800,
+    Defesa: 800,
+    Magia: 000,
+    Inteligência: 1000
+  }
+};
+
+var carta10 = {
+  nome: "Frosta",
+  fala: "I'm eleven and three quarters.",
+  imagem:
+    "https://i.pinimg.com/originals/d4/11/61/d4116107f88d32d49e53d1bd1cea8539.jpg",
+  atributos: {
+    Ataque: 800,
+    Defesa: 700,
+    Magia: 800,
+    Inteligência: 800
+  }
+};
+
+var carta11 = {
+  nome: "Huntara",
+  fala: "Don't annoy me when I eat.",
+  imagem:
+    "https://i.pinimg.com/474x/f7/65/e2/f765e2a4486c90c033e6c69f6a79712d.jpg",
+  atributos: {
+    Ataque: 700,
+    Defesa: 700,
+    Magia: 000,
+    Inteligência: 700
+  }
+};
+
+var carta12 = {
+  nome: "Hordak",
+  fala: "I am Hordak, and I defy your will!",
+  imagem: "https://wallpapercave.com/wp/wp8392793.png",
+  atributos: {
+    Ataque: 600,
+    Defesa: 500,
+    Magia: 000,
+    Inteligência: 800
+  }
+};
+
+var carta13 = {
+  nome: "Horde Prime",
+  fala: "All creatures have a place in service of Horde Prime.",
+  imagem:
+    "https://64.media.tumblr.com/db613bda893a53ff202e4999f93a22a4/3a758b87b678d91d-f5/s640x960/c7a593dcdb522058d644e8497be9c045af55c4c4.jpg",
+  atributos: {
+    Ataque: 800,
+    Defesa: 600,
+    Magia: 000,
+    Inteligência: 900
+  }
+};
+
+var carta14 = {
+  nome: "Kyle",
+  fala: "I get in trouble for everything around here.",
+  imagem:
+    "https://64.media.tumblr.com/be198be51860164ddd281c68f8518655/6184fd63abead516-12/s1280x1920/85a824933ff46fd6081430a399692812579110d8.jpg",
+  atributos: {
+    Ataque: 100,
+    Defesa: 100,
+    Magia: 000,
+    Inteligência: 100
+  }
+};
+
+var carta15 = {
+  nome: "Lonnie",
+  fala: "We got this.",
+  imagem: "https://pbs.twimg.com/media/Ea-BtMiWoAIxOlE.jpg",
+  atributos: {
+    Ataque: 500,
+    Defesa: 500,
+    Magia: 000,
+    Inteligência: 500
+  }
+};
+
+var carta16 = {
+  nome: "Madame Razz",
+  fala: "Razzle Dazzle!",
+  imagem:
+    "https://64.media.tumblr.com/e63cb120fedea4e1a14fe23bf84d33cf/fdffde6ff703747b-3a/s640x960/2200ef6ad2a77d6d579cfd2620e9c68f29aea835.jpg",
+  atributos: {
+    Ataque: 000,
+    Defesa: 600,
+    Magia: 1000,
+    Inteligência: 900
+  }
+};
+
+var carta17 = {
+  nome: "Mara",
+  fala: "You deserve love, too.",
+  imagem:
+    "https://i.pinimg.com/originals/88/f0/d9/88f0d9c9b3c7ec2edb6a11b0bf292d1a.jpg",
+  atributos: {
+    Ataque: 950,
+    Defesa: 850,
+    Magia: 950,
+    Inteligência: 950
+  }
+};
+
+var carta18 = {
+  nome: "Mermista",
+  fala: "UGH!",
+  imagem:
+    "https://64.media.tumblr.com/8e9bee70d9aa533ba4369c5bb5e29c70/b6ab4e15646fdb5c-5b/s1280x1920/26299792683259d3b000df95cbafe8b4721bc294.jpg",
+  atributos: {
+    Ataque: 800,
+    Defesa: 800,
+    Magia: 800,
+    Inteligência: 850
+  }
+};
+
+var carta19 = {
+  nome: "Micah",
+  fala: "Nothing will stop me!",
+  imagem:
+    "https://i.pinimg.com/736x/aa/43/80/aa43806f30a443ccc137270fa636356f.jpg",
+  atributos: {
+    Ataque: 900,
+    Defesa: 900,
+    Magia: 900,
+    Inteligência: 850
+  }
+};
+
+var carta20 = {
+  nome: "Netossa",
+  fala: "Boom! Nets!",
+  imagem:
+    "https://i.pinimg.com/originals/f2/25/73/f225736d40bfb459d039c2bc57304fc9.jpg",
+  atributos: {
+    Ataque: 700,
+    Defesa: 700,
+    Magia: 750,
+    Inteligência: 800
+  }
+};
+
+var carta21 = {
+  nome: "Perfuma",
+  fala: "Friendship isn't a weakness... it's my greatest strength.",
+  imagem:
+    "https://i.pinimg.com/736x/95/bd/0f/95bd0f1ef9afa30e515560f60f8581c3.jpg",
+  atributos: {
+    Ataque: 800,
+    Defesa: 700,
+    Magia: 800,
+    Inteligência: 750
+  }
+};
+
+var carta22 = {
+  nome: "Rogelio",
+  fala: "Rawh!",
+  imagem:
+    "http://pm1.narvii.com/7300/8434494f1699624b37f6d2b5dd033bd6e4f6be90r1-348-328v2_00.jpg",
+  atributos: {
+    Ataque: 400,
+    Defesa: 500,
+    Magia: 000,
+    Inteligência: 500
+  }
+};
+
+var carta23 = {
+  nome: "Scorpia",
+  fala: "I am brave, strong, loyal, and give great hugs. ",
+  imagem:
+    "https://i.pinimg.com/736x/44/2a/b2/442ab2135f3f5acdf68d91753ee7a42e.jpg",
+  atributos: {
+    Ataque: 800,
+    Defesa: 800,
+    Magia: 800,
+    Inteligência: 700
+  }
+};
+
+var carta24 = {
+  nome: "Sea Hawk",
+  fala: "Adventure!",
+  imagem:
+    "https://i.pinimg.com/736x/a6/dc/a8/a6dca8ea935cdde3dfb45045c32d7009.jpg",
+  atributos: {
+    Ataque: 700,
+    Defesa: 600,
+    Magia: 000,
+    Inteligência: 500
+  }
+};
+
+var carta25 = {
+  nome: "Shadow Weaver",
+  fala: "In order to catch a rat, you need to spring a trap.",
+  imagem:
+    "https://i.pinimg.com/736x/ca/cc/ba/caccba332ae114f0d4cc892f0e1ee000.jpg",
+  atributos: {
+    Ataque: 800,
+    Defesa: 800,
+    Magia: 900,
+    Inteligência: 800
+  }
+};
+
+var carta26 = {
+  nome: "Swift Wind",
+  fala: "I am my own horse.",
+  imagem:
+    "https://i.pinimg.com/originals/c6/72/1e/c6721e61474cdd6cfbf1966152482b33.jpg",
+  atributos: {
+    Ataque: 500,
+    Defesa: 600,
+    Magia: 700,
+    Inteligência: 600
+  }
+};
+
+var carta27 = {
+  nome: "Wrong Hordak",
+  fala: "I put amniotic fluid in there... that was a joke.",
+  imagem:
+    "https://i.pinimg.com/originals/db/e8/7e/dbe87e35fd3d1499f5b1b2a9439a442f.jpg",
+  atributos: {
+    Ataque: 200,
+    Defesa: 200,
+    Magia: 000,
+    Inteligência: 200
+  }
+};
+
+var carta28 = {
+  nome: "Spinnerella",
+  fala: "Yes, darling, you have beautiful nets.",
+  imagem:
+    "https://i.pinimg.com/originals/ac/5b/5e/ac5b5ea446f6aed23dc7955341ce2f84.jpg",
+  atributos: {
+    Ataque: 750,
+    Defesa: 650,
+    Magia: 750,
+    Inteligência: 700
+  }
+};
+
+var carta29 = {
+  nome: "Catra e Melog",
+  fala: "Miau! ❤️",
+  imagem:
+    "https://i.pinimg.com/736x/95/7a/06/957a068abb8bd055ad149903661ead0a.jpg",
+  atributos: {
+    Ataque: 800,
+    Defesa: 700,
+    Magia: 800,
+    Inteligência: 900
+  }
+};
+
+var carta30 = {
+  nome: "Entrapta e Emily",
+  fala: "Beep beep! ❤️",
+  imagem:
+    "https://pbs.twimg.com/profile_images/1261017864498302978/fFlGz0c2_400x400.jpg",
+  atributos: {
+    Ataque: 850,
+    Defesa: 850,
+    Magia: 000,
+    Inteligência: 1000
+  }
+};
+
+var carta31 = {
+  nome: "Light Hope",
+  fala: "This is our destiny.",
+  imagem: "https://pbs.twimg.com/media/EIoVxUBWsAAxDKP.jpg",
+  atributos: {
+    Ataque: 800,
+    Defesa: 800,
+    Magia: 000,
+    Inteligência: 900
+  }
+};
+
+var carta32 = {
+  nome: "Star Siblings",
+  fala: "You Etherians are so reckless.",
+  imagem:
+    "https://64.media.tumblr.com/a88575bd3cc4afd7a1718f603d4cb6d6/ea41d2796566694b-b8/s500x750/77eae19ad5f4ccba9c3dde6f571d5d5044ffe328.png",
+  atributos: {
+    Ataque: 850,
+    Defesa: 850,
+    Magia: 850,
+    Inteligência: 800
+  }
+};
+
+// Lista com as cartas
+
+var cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20,
+  carta21,
+  carta22,
+  carta23,
+  carta24,
+  carta25,
+  carta26,
+  carta27,
+  carta28,
+  carta29,
+  carta30,
+  carta31,
+  carta32
+];
+
+// Variáveis iniciais
+
+var cartaJogador = 0;
+var cartaComputador = 0;
+var resultado = document.getElementById("resultado");
+var placar = [0, 0];
+exibirPlacar();
+
+// Função para sortear a carta
+
+function Sortearcarta() {
+  zerarCarta("carta-jogador");
+  zerarCarta("carta-computador");
+  resultado.innerHTML = "";
+  var i = cartas.length;
+  var cartaSorteadaComputador = Math.floor(Math.random() * i);
+  cartaComputador = cartas[cartaSorteadaComputador];
+  var cartaSorteadaJogador = Math.floor(Math.random() * i);
+
+  while (cartaSorteadaJogador == cartaSorteadaComputador) {
+    var cartaSorteadaJogador = Math.floor(Math.random() * i);
+  }
+  cartaJogador = cartas[cartaSorteadaJogador];
+  document.getElementById("btnJogar").disabled = false;
+  document.getElementById("btnSortear").disabled = true;
+
+  exibirCartaJogador();
+}
+
+// Função para extrair o atributo selecionado
+
+function atributoEscolhido() {
+  var selecionado = document.getElementsByName("atributo");
+
+  for (var i = 0; i < selecionado.length; i++) {
+    if (selecionado[i].checked == true) {
+      return selecionado[i].value;
+    }
+  }
+}
+
+// Função para jogar
+
+function jogar() {
+  var atributoMarcado = atributoEscolhido();
+  var valorCartaJogador = cartaJogador.atributos[atributoMarcado];
+  var valorCartaComputador = cartaComputador.atributos[atributoMarcado];
+  var pontosJogador = 0;
+  var pontosComputador = 0;
+
+  while (isNaN(atributoMarcado)) {
+    if (valorCartaJogador > valorCartaComputador) {
+      placar[0]++;
+      htmlResultado = `<p class='resultado-final'>Parabéns! Você venceu! ⚔️</p><br><img src="https://64.media.tumblr.com/2a2df6f48b1072558ba7388586624c40/tumblr_inline_pj4mzdHNjy1rict0a_540.gifv">'`;
+      break;
+    } else if (valorCartaJogador < valorCartaComputador) {
+      placar[1]++;
+      htmlResultado = `<p class='resultado-final'> Ops! Você perdeu! Tente novamente!</p><br><img src="https://c.tenor.com/mksT8jk3cNMAAAAC/ugh-mermista.gif">'`;
+      break;
+    } else if (isNaN(valorCartaJogador) || isNaN(valorCartaComputador)) {
+      htmlResultado = `<p class='resultado-final'> Por favor, selecione um atributo!</p><br><img src="https://c.tenor.com/LKcJO3TmRtsAAAAM/shera-adora.gif">'`;
+      resultado.innerHTML = htmlResultado;
+      return;
+    } else {
+      placar[0]++;
+      placar[1]++;
+      htmlResultado = `<p class='resultado-final'> Empate! 🛡️</p><br><img src="https://i.pinimg.com/originals/d2/28/f3/d228f316b83894cf86cb73b394d4a509.gif">'`;
+      break;
+    }
+  }
+  resultado.innerHTML = htmlResultado;
+  document.getElementById("btnJogar").disabled = true;
+  document.getElementById("btnSortear").disabled = false;
+  exibirCartaComputador();
+  exibirPlacar();
+}
+
+// Função para mostrar a imagem do jogador
+
+function exibirCartaJogador() {
+  var cartaJogadorImagem = document.getElementById("carta-jogador");
+  cartaJogadorImagem.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  var moldura =
+    '<img src="https://i.pinimg.com/originals/4e/49/06/4e490632804acd7d22a85d0bcf3e319e.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = `<div id=opcoes class=carta-status>`;
+  var texto = "";
+  for (var atributo in cartaJogador.atributos) {
+    texto +=
+      "&nbsp;<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>&nbsp;" +
+      "" +
+      atributo +
+      " " +
+      ": " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+    var nome = `<p class=carta-subtitle>${cartaJogador.nome}</p>`;
+    var frase = `<p class=carta-subtitle2>${cartaJogador.fala}</p>`;
+    cartaJogadorImagem.innerHTML =
+      moldura + nome + frase + tagHTML + texto + `</div>`;
+  }
+}
+
+function exibirCartaComputador() {
+  var cartaComputadorImagem = document.getElementById("carta-computador");
+  cartaComputadorImagem.style.backgroundImage = `url(${cartaComputador.imagem})`;
+  var moldura =
+    '<img src="https://i.pinimg.com/originals/4e/49/06/4e490632804acd7d22a85d0bcf3e319e.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = `<div id=opcoes class=carta-status2>`;
+  var texto = "";
+  for (var atributo in cartaComputador.atributos) {
+    texto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      "&nbsp;&nbsp;" +
+      atributo +
+      " " +
+      ": " +
+      cartaComputador.atributos[atributo];
+    var nome = `<p class=carta-subtitle>${cartaComputador.nome}</p>`;
+    var frase = `<p class=carta-subtitle2>${cartaComputador.fala}</p>`;
+    cartaComputadorImagem.innerHTML =
+      moldura + nome + frase + tagHTML + texto + `</div>`;
+  }
+}
+
+// Função para zerar cartas da tela
+
+function zerarCarta(id) {
+  var idCarta = document.getElementById(id);
+  var idCartaTexto =
+    '<img src="https://cinemaplanet.pt/wp-content/uploads/2020/06/sheraposter.jpg" style=" width: inherit; height: inherit; position: absolute;">';
+  idCarta.innerHTML = idCartaTexto;
+}
+
+// Função para zerar placar
+
+function Zerar() {
+  placar[0] = 0;
+  placar[1] = 0;
+  exibirPlacar();
+}
+
+// Função para exibir placar
+
+function exibirPlacar() {
+  var placarJogador = document.getElementById("placarJogador");
+  var placarComputador = document.getElementById("placarComputador");
+
+  placarJogador.innerHTML = `👑 Jogador: ${placar[0]}`;
+  placarComputador.innerHTML = `👾 Computador: ${placar[1]}`;
+}
